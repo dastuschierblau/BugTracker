df6b24ba3fd6d17a02688a9a3dcc0af71fc1d6b6
Emmett <emmett.slack@yahoo.com>
2020-03-20 17:14:26 -0500

    Fixed another wrong directory name in server.js

--- server.js
+++ server.js
@@ -29,10 +29,10 @@ app.use('/api/projects', require('./routes/apis/projects'));
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static('client/dist'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/client/dist/index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
   });
 }
 
