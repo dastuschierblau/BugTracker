668c4eec9b2fdb19dd1977e8b955e3e2afb9e986
Emmett <emmett.slack@yahoo.com>
2020-03-20 17:09:24 -0500

    Added index.html to file path in server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/client/dist'));
+    res.sendFile(path.join(__dirname, '/client/dist/index.html'));
   });
 }
 
