68338bcf6a8464f4fd5bd09ee6c3f628ed050949
Emmett <emmett.slack@yahoo.com>
2020-03-20 17:07:30 -0500

    Fixed incorrect directory naming for root application

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/client/build'));
+    res.sendFile(path.join(__dirname, '/client/dist'));
   });
 }
 
